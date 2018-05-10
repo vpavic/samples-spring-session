85f8254bfba5601bafc973184090a6e3002d282c
Vedran Pavic <vedran.pavic@gmail.com>
2018-05-10 23:09:35 +0200

    Polish Hazelcast demo

---
+++ httpsession-hazelcast/src/main/java/demo/HazelcastConfiguration.java
@@ -0,0 +1,30 @@
+package demo;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.MapAttributeConfig;
+import com.hazelcast.config.MapIndexConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.hazelcast.HazelcastSessionRepository;
+import org.springframework.session.hazelcast.PrincipalNameExtractor;
+
+@Configuration
+public class HazelcastConfiguration {
+
+	@Bean
+	public Config config() {
+		MapAttributeConfig attributeConfig = new MapAttributeConfig()
+				.setName(HazelcastSessionRepository.PRINCIPAL_NAME_ATTRIBUTE)
+				.setExtractor(PrincipalNameExtractor.class.getName());
+
+		Config config = new Config();
+
+		config.getMapConfig(HazelcastSessionRepository.DEFAULT_SESSION_MAP_NAME)
+				.addMapAttributeConfig(attributeConfig)
+				.addMapIndexConfig(new MapIndexConfig(
+						HazelcastSessionRepository.PRINCIPAL_NAME_ATTRIBUTE, false));
+
+		return config;
+	}
+
+}
--- httpsession-hazelcast/src/main/java/demo/HttpSessionHazelcastApplication.java
+++ httpsession-hazelcast/src/main/java/demo/HttpSessionHazelcastApplication.java
@@ -2,19 +2,13 @@ package demo;
 
 import javax.servlet.http.HttpSession;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.config.MapAttributeConfig;
-import com.hazelcast.config.MapIndexConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.session.hazelcast.HazelcastSessionRepository;
-import org.springframework.session.hazelcast.PrincipalNameExtractor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
+@RestController
 public class HttpSessionHazelcastApplication {
 
 	public static void main(String[] args) {
@@ -24,22 +18,6 @@ public class HttpSessionHazelcastApplication {
 	@GetMapping(path = "/")
 	public String home(HttpSession session) {
 		return session.getId();
-	}
-
-	@Bean
-	public Config config() {
-		MapAttributeConfig attributeConfig = new MapAttributeConfig()
-				.setName(HazelcastSessionRepository.PRINCIPAL_NAME_ATTRIBUTE)
-				.setExtractor(PrincipalNameExtractor.class.getName());
-
-		Config config = new Config();
-
-		config.getMapConfig(HazelcastSessionRepository.DEFAULT_SESSION_MAP_NAME)
-				.addMapAttributeConfig(attributeConfig)
-				.addMapIndexConfig(new MapIndexConfig(
-						HazelcastSessionRepository.PRINCIPAL_NAME_ATTRIBUTE, false));
-
-		return config;
 	}
 
 }
