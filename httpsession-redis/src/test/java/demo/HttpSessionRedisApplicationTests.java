e487f3f11b89aae75de5c1ec378c6b0ea2e41b87
Vedran Pavic <vedran.pavic@gmail.com>
2018-06-01 09:17:48 +0200

    Improve integration tests

--- httpsession-redis/src/test/java/demo/HttpSessionRedisApplicationTests.java
+++ httpsession-redis/src/test/java/demo/HttpSessionRedisApplicationTests.java
@@ -1,44 +1,34 @@
 package demo;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.GenericContainer;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ContextConfiguration(initializers = HttpSessionRedisApplicationTests.Initializer.class)
 public class HttpSessionRedisApplicationTests {
-
-	private static GenericContainer container = new GenericContainer("redis:4.0.9").withExposedPorts(6379);
-
-	@BeforeClass
-	public static void setUpClass() {
-		container.start();
-	}
-
-	@AfterClass
-	public static void tearDownClass() {
-		container.stop();
-	}
 
 	@Test
 	public void contextLoads() {
 	}
 
-	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+	@TestConfiguration
+	static class Config {
 
-		@Override
-		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-			TestPropertyValues.of("spring.redis.host=" + container.getContainerIpAddress(), "spring.redis.port=" +
-					container.getFirstMappedPort()).applyTo(configurableApplicationContext.getEnvironment());
+		@Bean(initMethod = "start")
+		GenericContainer redisContainer() {
+			return new GenericContainer("redis:4.0.9").withExposedPorts(6379);
+		}
+
+		@Bean
+		LettuceConnectionFactory redisConnectionFactory() {
+			return new LettuceConnectionFactory(redisContainer().getContainerIpAddress(),
+					redisContainer().getFirstMappedPort());
 		}
 
 	}
