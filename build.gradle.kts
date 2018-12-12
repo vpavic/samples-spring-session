5c1a0670c058a30827d6b8221d6e10b9bfb16262
Vedran Pavic <vedran.pavic@gmail.com>
2018-12-13 00:01:28 +0100

    Add DevTools

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,12 +19,22 @@ subprojects {
 		mavenCentral()
 	}
 
+	val developmentOnly: Configuration by configurations.creating
+
+	configurations {
+		"runtimeClasspath" {
+			extendsFrom(configurations["developmentOnly"])
+		}
+	}
+
 	dependencies {
 		"implementation"("org.springframework.boot:spring-boot-starter-actuator")
 		"implementation"("org.springframework.boot:spring-boot-starter-mustache")
 		"implementation"("org.springframework.boot:spring-boot-starter-security")
 
 		"compileOnly"("org.springframework.session:spring-session-core")
+
+		developmentOnly("org.springframework.boot:spring-boot-devtools")
 
 		"testImplementation"("org.springframework.boot:spring-boot-starter-test")
 	}
