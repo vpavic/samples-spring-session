c89382c42bff8b38fefe37cfba0875d19fd7379f
Vedran Pavic <vedran.pavic@gmail.com>
2019-04-16 19:55:36 +0200

    Improve WebSession demo

--- demo-websession/src/main/java/demo/DemoWebSessionApplication.java
+++ demo-websession/src/main/java/demo/DemoWebSessionApplication.java
@@ -1,23 +1,35 @@
 package demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
+import org.springframework.session.ReactiveSessionRepository;
+import org.springframework.session.Session;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.WebSession;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class DemoWebSessionApplication {
+
+	@Autowired
+	private ReactiveSessionRepository<? extends Session> sessionRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoWebSessionApplication.class, args);
 	}
 
-	@GetMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
-	public String home(WebSession session) {
-		return session.getId();
+	@GetMapping(path = "/")
+	public String home(WebSession webSession, Model model) {
+		model.addAttribute("sessionRepositoryType", this.sessionRepository.getClass().getName());
+		model.addAttribute("sessionId", webSession.getId());
+		model.addAttribute("sessionCreationTime", webSession.getCreationTime());
+		model.addAttribute("sessionLastAccessedTime", webSession.getLastAccessTime());
+		model.addAttribute("sessionMaxInactiveInterval", webSession.getMaxIdleTime());
+		model.addAttribute("sessionAttributes", webSession.getAttributes().keySet());
+		return "home";
 	}
 
 }
