10c6043848c84c996a322a4e468356a4c7905a9c
Vedran Pavic <vedran.pavic@gmail.com>
2020-01-24 07:14:23 +0100

    Migrate build to Groovy DSL

---
+++ buildSrc/src/main/java/sample/build/ConventionsPlugin.java
@@ -0,0 +1,35 @@
+package sample.build;
+
+import java.util.Map;
+
+import org.gradle.api.JavaVersion;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.plugins.JavaPluginExtension;
+
+public class ConventionsPlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project project) {
+		project.getPlugins().withType(JavaPlugin.class, javaPlugin -> {
+			JavaPluginExtension javaPluginExtension = project.getExtensions().getByType(JavaPluginExtension.class);
+			javaPluginExtension.setSourceCompatibility(JavaVersion.VERSION_11);
+			Map.Entry<String, Configuration> redis = Map.entry("redis",
+					project.getConfigurations().create("redisImplementation"));
+			Map<String, Configuration> configurations = Map.ofEntries(redis,
+					Map.entry("jdbc", project.getConfigurations().create("jdbcImplementation")),
+					Map.entry("hazelcast", project.getConfigurations().create("hazelcastImplementation")),
+					Map.entry("mongoDb", project.getConfigurations().create("mongoDbImplementation")));
+			String profile = (String) project.findProperty("profile");
+			if (profile == null) {
+				profile = redis.getKey();
+			}
+			Configuration active = configurations.getOrDefault(profile, redis.getValue());
+			project.getConfigurations().getByName("implementation", configuration ->
+					configuration.extendsFrom(active));
+		});
+	}
+
+}
