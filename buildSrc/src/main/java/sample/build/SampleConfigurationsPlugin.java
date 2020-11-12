736e3946815027acd8d422ed5b833823204d4f0e
Vedran Pavic <vedran.pavic@gmail.com>
2020-11-12 22:27:59 +0100

    Improve sample configurations plugin

--- buildSrc/src/main/java/sample/build/SampleConfigurationsPlugin.java
+++ buildSrc/src/main/java/sample/build/SampleConfigurationsPlugin.java
@@ -1,7 +1,9 @@
 package sample.build;
 
 import java.util.Map;
+import java.util.Map.Entry;
 
+import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
@@ -12,19 +14,29 @@ class SampleConfigurationsPlugin implements Plugin<Project> {
 	@Override
 	public void apply(Project project) {
 		project.getPlugins().withType(JavaPlugin.class, javaPlugin -> {
-			Map.Entry<String, Configuration> redis = Map.entry("redis",
+			Entry<String, Configuration> redisImplementation = Map.entry("redis",
 					project.getConfigurations().create("redisImplementation"));
-			Map<String, Configuration> configurations = Map.ofEntries(redis,
+			Map<String, Configuration> implementationConfigurations = Map.ofEntries(redisImplementation,
 					Map.entry("jdbc", project.getConfigurations().create("jdbcImplementation")),
 					Map.entry("hazelcast", project.getConfigurations().create("hazelcastImplementation")),
 					Map.entry("mongoDb", project.getConfigurations().create("mongoDbImplementation")));
+			Map<String, Configuration> runtimeConfigurations = Map.ofEntries(
+					Map.entry("jdbc", project.getConfigurations().create("jdbcRuntime")));
 			String profile = (String) project.findProperty("profile");
 			if (profile == null) {
-				profile = redis.getKey();
+				profile = redisImplementation.getKey();
 			}
-			Configuration active = configurations.getOrDefault(profile, redis.getValue());
+			Configuration activeImplementation = implementationConfigurations.get(profile);
+			if (activeImplementation == null) {
+				throw new GradleException("Invalid profile: " + profile);
+			}
 			project.getConfigurations().getByName("implementation", configuration ->
-					configuration.extendsFrom(active));
+					configuration.extendsFrom(activeImplementation));
+			Configuration activeRuntime = runtimeConfigurations.get(profile);
+			if (activeRuntime != null) {
+				project.getConfigurations().getByName("runtime", configuration ->
+						configuration.extendsFrom(activeRuntime));
+			}
 		});
 	}
 
