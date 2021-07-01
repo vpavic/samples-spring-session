d6c483d2c53889568621e724e350bbb6a86c3439
Vedran Pavic <vedran.pavic@gmail.com>
2021-07-01 21:45:57 +0200

    Update HttpSession JDBC sample
    - default to PostgreSQL
    - remove H2

--- sample-httpsession-jdbc/src/test/java/sample/httpsession/SampleHttpSessionJdbcApplicationTests.java
+++ sample-httpsession-jdbc/src/test/java/sample/httpsession/SampleHttpSessionJdbcApplicationTests.java
@@ -1,15 +1,31 @@
 package sample.httpsession;
 
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.session.jdbc.JdbcIndexedSessionRepository;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Testcontainers
 class SampleHttpSessionJdbcApplicationTests {
+
+	@Container
+	private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:13.3-alpine");
+
+	@DynamicPropertySource
+	static void dataSourceProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", postgresContainer::getUsername);
+		registry.add("spring.datasource.password", postgresContainer::getPassword);
+	}
 
 	@Test
 	void contextLoads(ApplicationContext context) {
