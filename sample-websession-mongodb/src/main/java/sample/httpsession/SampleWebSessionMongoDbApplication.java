afe4c86e6057a66fbf05253020a244ae0e92d264
Vedran Pavic <vpavic@duck.com>
2022-08-24 17:41:46 +0200

    Disable proxyBeanMethods in all configurations

--- sample-websession-mongodb/src/main/java/sample/httpsession/SampleWebSessionMongoDbApplication.java
+++ sample-websession-mongodb/src/main/java/sample/httpsession/SampleWebSessionMongoDbApplication.java
@@ -3,7 +3,7 @@ package sample.httpsession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class SampleWebSessionMongoDbApplication {
 
 	public static void main(String[] args) {
